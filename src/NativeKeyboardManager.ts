6b6ce9ff7eb894c0cfca9a05f37c1a9634d08400
derekrsargent <derek.r.sargent@gmail.com>
2023-05-16 15:54:03 -0400

    docs: add comments

--- src/NativeKeyboardManager.ts
+++ src/NativeKeyboardManager.ts
@@ -2,6 +2,12 @@ import type { TurboModule } from 'react-native';
 import { TurboModuleRegistry } from 'react-native';
 
 export interface Spec extends TurboModule {
+  /* 
+  These are needed here even though `addListener` is called on the 
+  NativeEventEmitter (which already has an `addListener` method available) 
+  and not the Module itself. If these are not added then the event won't be 
+  received on the React Native side. 
+  */
   addListener(eventName: string): void;
   removeListeners(count: number): void;
 }

0db7cfbcf78cb7984cc589fc3990784fa788796b
derekrsargent <derek.r.sargent@gmail.com>
2023-05-18 19:12:16 -0400

    feat: add methods for turbomodule android

--- android/src/newarch/KeyboardManagerSpec.kt
+++ android/src/newarch/KeyboardManagerSpec.kt
@@ -4,4 +4,13 @@ import com.facebook.react.bridge.ReactApplicationContext
 
 abstract class KeyboardManagerSpec internal constructor(context: ReactApplicationContext) :
   NativeKeyboardManagerSpec(context) {
+
+  /*
+  We could implement these in the Module instead (KeyboardManagerModule.kt),
+  but since they are never called we will implement them here to not pollute
+  the Module. The reason these are never called is because we call `addListener`
+  from the NativeEventEmitter for the Module instead of on the Module itself.
+  */
+  override fun addListener(eventName: String) {}
+  override fun removeListeners(count: Double) {}
 }
